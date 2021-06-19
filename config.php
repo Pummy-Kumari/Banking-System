060ab1d7059c756c3b196fa3c0712c55f517f011
Pummy-Kumari <86144210+Pummy-Kumari@users.noreply.github.com>
2021-06-19 13:03:33 +0530

    Add files via upload
    # the-sparks-foundation
    Sparks Foundation Internship Project : Basic Banking System  
    A Web Application used to transfer money between multiple users (Project contains 10 dummy users). A dummy user can also be created.  
    
    Stack used - 
    Front-end : HTML, CSS, Bootstrap & Javascript 
    Back-end : PHP 
    Database : MySQL   
    
    Database contains two Tables- Users Table & Transaction Table 
    1. User table have basic fields such as name, email & current balance. 
    2. Transaction table records all transfers happened along with their time.  
    
    Flow of the Website: Home Page > View all Users > Select and View one User > Transfer Money > Select reciever > View all Users > View Transfer History.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_name="sparks_bank";
+$db_port=3306;
+       
+         $conn=mysqli_connect($db_host,$db_user,$db_password,$db_name,$db_port);
+
+		 if(!$conn){
+			 die("could not connect to the database due to the following error --> ".mysqli_connect_error());
+		 }
+?>
